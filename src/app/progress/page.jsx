5aba68e5e84b7a5234e88af781523ba7053a2eb7
DrTobboggan <109244105+DrTobboggan@users.noreply.github.com>
2023-05-25 08:59:41 -0600

    Progress Page- Reverted to Prisma client for results.

--- src/app/progress/page.jsx
+++ src/app/progress/page.jsx
@@ -3,31 +3,33 @@ import React from "react";
 import prisma from "../../../public/helpers/Client";
 export const revalidate = 0;
 
-const baseUrl = process.env.API_BASE_URL || "http://localhost:3000";
+// const baseUrl = process.env.API_BASE_URL || "http://localhost:3000";
 import ProgressTable from "../../../components/ProgressTable";
 async function getProgressData() {
   const targets = await prisma.targets.findMany({ orderBy: { id: "asc" } });
-  let results;
-  try {
-    results = await fetch(`${baseUrl}/api/results`, {
-      method: "POST",
-      cache: "no-store",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ next: { tags: ["resultData"] } }),
-    }).then((res) => res.json());
-  } catch (error) {
-    console.error(error);
-    try {
-      results = await prisma.results.findMany({ orderBy: { id: "asc" } });
-    } catch (error) {
-      console.error(error);
-    }
-  }
+  const results = await prisma.results.findMany({ orderBy: { id: "asc" } });
   const forecast = await prisma.month_forecast.findMany({
     orderBy: { id: "asc" },
   });
+  // let results;
+  // try {
+  //   results = await fetch(`${baseUrl}/api/results`, {
+  //     method: "POST",
+  //     cache: "no-store",
+  //     headers: {
+  //       "Content-Type": "application/json",
+  //     },
+  //     body: JSON.stringify({ next: { tags: ["resultData"] } }),
+  //   }).then((res) => res.json());
+  // } catch (error) {
+  //   console.error(error);
+  //   try {
+  //     results = await prisma.results.findMany({ orderBy: { id: "asc" } });
+  //   } catch (error) {
+  //     console.error(error);
+  //   }
+  // }
+
   return { targets, results, forecast };
 }
 
